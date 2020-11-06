5c13e0dbb9fffeb6986c6474d5fda7938701ec76
Nikhil M Kumar <38357711+theNikhilMannem@users.noreply.github.com>
2020-11-06 16:15:12 +0530

    Create 07_Connect-Redux-to-React.js

---
+++ 07_Connect-Redux-to-React.js
@@ -0,0 +1,42 @@
+/*
+
+
+
+*/
+
+
+
+const addMessage = (message) => {
+  return {
+    type: 'ADD',
+    message: message
+  }
+};
+
+const mapStateToProps = (state) => {
+  return {
+    messages: state
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    submitNewMessage: (message) => {
+      dispatch(addMessage(message));
+    }
+  }
+};
+
+class Presentational extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <h3>This is a Presentational Component</h3>
+  }
+};
+
+const connect = ReactRedux.connect;
+
+// Assigning the value of the connect() to a ConnectedComponent: after calling the Presentational component right (immediately) after the connect() method.
+const ConnectedComponent = connect(mapStateToProps, mapDispatchToProps) (Presentational);
